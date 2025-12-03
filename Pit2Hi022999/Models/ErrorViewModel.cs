98a6f9222b2f3c2ae18ff5d2b85b829ecc5fa648
Horishin01 <75874125+Horishin01@users.noreply.github.com>
2025-12-03 15:37:19 +0900

    更新

--- Pit2Hi022999/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Pit2Hi022999.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
